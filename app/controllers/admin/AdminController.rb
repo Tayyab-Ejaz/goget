e80c9b4cf8d046c698cc95ef13a2f02d82e64101
tayyabejaz668 <tayyabejaz668@gmail.com>
2022-06-23 12:07:13 +0500

    - Admin Api - Jobs Api - Claim job Api - Execute job Api

--- app/controllers/admin/AdminController.rb
+++ app/controllers/admin/AdminController.rb
@@ -1,16 +1,17 @@
-class ApplicationController < ActionController::API
+class AdminController < ApplicationController
     include JsonWebToken
 
-    before_action :authenticate
+    skip_before_action :authenticate
+    before_action :authenticate_admin
 
     private
-    def authenticate
+    def authenticate_admin
         begin
             header = request.headers['Authorization']
             header = header&.split(" ")&.last
             decoded = jwt_decode(header)
             @current_user = User.find_by(id: decoded[:user_id])
-            render status: 403, json: { message: "Invalid Auth Token" } unless @current_user
+            render status: 403, json: { message: "Un Authorized" } unless @current_user&.admin
         rescue
             render status: 400, json: { message: "Invalid Auth Token" }
         end
